0076a039c1a3f7423647c302108d73ac2e2f3c26
Ian Collins <ian@powerset.com>
2010-02-01 14:44:31 -0800

    headers changed up, no more lighter/bolder, features list started

--- js/app.js
+++ js/app.js
@@ -325,7 +325,7 @@ window.addEvent('domready', function(){
         initial_limit: 32,		    
         // user_name    : 'ninjacam',
         json_opts: { 
-          data : { q : 'yfrog' },
+          data : { q : 'tweetphoto' },
           onFailure: function(){
             $('twitter-and-flickr').removeClass('loading').addClass('failed');
           }
