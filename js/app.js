be38a03f0034036aa19e8c0f34dabbbacbcac5fc
Ian Collins <ian@powerset.com>
2010-02-22 20:54:11 -0800

    fix

--- js/app.js
+++ js/app.js
@@ -290,7 +290,7 @@ window.addEvent('domready', function(){
           }
         });
         
-        $('archives-link').setStyle('display','block').delay(5000, $('archives-link'));
+        $('archives-link').setStyle.delay(5000, $('archives-link'), ['display','block']);
         if (document.referrer.test("tumblr.com"))
           $('tumblr-buttons').setStyle('display','block');
 		  }
